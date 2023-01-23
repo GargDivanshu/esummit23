7fde904f1597d3546eb84672651e9f5c86d91a9a
aaditya9899 <aadityaprasad9899@gmail.com>
2023-01-23 11:21:33 +0530

    past speakers added

--- components/PastSpeakers.tsx
+++ components/PastSpeakers.tsx
@@ -13,20 +13,24 @@ import {
 export default function () {
   return (
     <>
+    
+    <div className="font-poppins bg-[url('https://img.freepik.com/premium-photo/cargo-space-craft-earth-planet-dark-background-sci-fi-wallpaper-space-station-orbiting-earth-space-ship-space-art-wallpaper-solar-observatory-elements-this-image-furnished-by-nasa-3d-illustration_150455-15545.jpg?w=2000')] bg-cover bg-center bg-scroll 2xl:container 2xl:mx-auto w-screen">
+      <div className="bg-background/90 w-screen">
+        <h1 className="font-poppins text-2xl text-center  lg:text-5xl font-bold text-white ease-in duration-200 bg-gradient-to-tr from-pink1 to-pink2  p-5 pl-10">
+          Our Past Speakers
+        </h1>
+
+
+
+
     <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-      <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
-        <div>
-          <h1 className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
-            OUR PAST SPEAKERS
-          </h1>
-        </div>
-        </div>
+      
        
       <div className="grid gap-10 sm:grid-cols-2 lg:grid-cols-4">
         <div>
-          <div className="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
+          <div className="relative overflow-hidden transition duration-300  transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
             <img
-              className="object-cover w-full h-56 md:h-64 xl:h-80"
+              className="object-cover w-full h-56 md:h-64 xl:h-80 "
               src="https://images.moneycontrol.com/static-mcnews/2021/08/Ashneer-Grover-2-770x433.jpg?impolicy=website&width=770&height=431"
               alt="Person"
             />
@@ -124,14 +128,14 @@ export default function () {
           <div className="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
             <img
               className="object-cover w-full h-56 md:h-64 xl:h-80"
-              src="https://i0.wp.com/biographyinsider.com/wp-content/uploads/2021/09/Ankur-Warikoo.jpeg?fit=1920%2C2400&ssl=1"
+              src="https://femina.wwmindia.com/content/2019/sep/chhayasharma11569824996.jpg"
               alt="Person"
             />
             <div className="absolute inset-0 flex flex-col justify-center px-5 py-4 text-center transition-opacity duration-300 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
               <p className="mb-1 text-lg font-bold text-gray-100">
-                Ankur Warikoo
+                Chhaya Sharma
               </p>
-              <p className="mb-4 text-xs text-gray-100">former CEO of Groupon India, co-founder & former CEO of Nearbuy</p>
+              <p className="mb-4 text-xs text-gray-100">IPS officer (cracked Nirbhaya case)</p>
               
             </div>
           </div>
@@ -154,6 +158,8 @@ export default function () {
         </div>
       </div>
     </div> 
+    </div>
+    </div>
     </>
   )
 }
